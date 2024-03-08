28a2d768c1d54b6d9e5b69e1635baa5287dde583
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2024-03-09 00:04:33 +0300

    feat: add removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -50,8 +50,17 @@ function mergeObjects(/* objects */) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const arr = Object.keys(obj);
+  const copyObj = { ...obj };
+
+  arr.forEach((key) => {
+    if (keys.includes(key)) {
+      delete copyObj[key];
+    }
+  });
+
+  return copyObj;
 }
 
 /**
